7de508877a32f712f5d41310ea73b32ba1a4e628
Chris Postma <cjrpostma@gmail.com>
2020-07-13 19:32:02 -0400

    Add login route and configure cookie-session.

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -25,10 +25,34 @@ router.get('/login', (req: Request, res: Response) => {
 router.post('/login', (req: RequestWithBody, res: Response) => {
   const { email, password } = req.body;
 
-  if (email) {
-    res.send(email + password);
+  if (
+    email &&
+    password &&
+    email === 'test@test.com' &&
+    password === 'password'
+  ) {
+    req.session = { loggedIn: true };
+    res.redirect('/');
   } else {
-    res.status(422).send('You must provide an email.');
+    res.send('Invalid email or password.');
+  }
+});
+
+router.get('/', (req: Request, res: Response) => {
+  if (req.session && req.session.loggedIn) {
+    res.send(`
+      <div>
+        <p>You are logged in.</p>
+        <a href="/logout">Logout</a>
+      </div>
+    `);
+  } else {
+    res.send(`
+      <div>
+        <p>You are not logged in.</p>
+        <a href="/login">Login</a>
+      </div>
+    `);
   }
 });
 
